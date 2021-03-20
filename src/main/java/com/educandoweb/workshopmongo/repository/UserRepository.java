024648f168e45783d3a1fb29f0a4579f195670c8
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2021-03-20 12:26:57 -0300

    Obtendo um usuário por id

--- src/main/java/com/educandoweb/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/educandoweb/workshopmongo/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.educandoweb.workshopmongo.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +7,5 @@ import com.educandoweb.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
-
 
 }
--- src/main/java/com/educandoweb/workshopmongo/services/UserService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.educandoweb.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.workshopmongo.domain.User;
 import com.educandoweb.workshopmongo.repository.UserRepository;
+import com.educandoweb.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,14 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		if (user.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user.get();
 	}
 
 }
